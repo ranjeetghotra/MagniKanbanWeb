34a8af1e2565ef2f98ecc5d6a3fc408a68720b39
ranje <ranje@RANJIT>
2022-12-16 18:13:56 +0530

    Add project files.

---
+++ MagniKanbanWeb/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using MagniKanbanWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MagniKanbanWeb.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController: ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            // var user = await userManager.FindByNameAsync(model.Username);
+            // if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            // {
+            // var userRoles = await userManager.GetRolesAsync(user);
+            if (model.Email == "test@mail.com" && model.Password == "1234")
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, model.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                // foreach (var userRole in userRoles)
+                // {
+                //     authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                // }
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
