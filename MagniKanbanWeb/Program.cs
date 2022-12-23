fe3f15d0c3af3b3c244bf4d1e36a2aa19c8acb28
shivam <shivamratta.7@gmail.com>
2022-12-23 12:51:31 +0530

    Added cors

--- MagniKanbanWeb/Program.cs
+++ MagniKanbanWeb/Program.cs
@@ -15,6 +15,10 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => { options.SignIn
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("corsapp");
 app.UseHttpsRedirection();
 app.UseAuthentication();;
 
