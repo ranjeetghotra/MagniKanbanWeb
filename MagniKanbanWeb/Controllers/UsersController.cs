e0dd58d69da85bf9a921374744749aeb3cde195a
Ranjit <ranjeetghotra6@gmail.com>
2022-12-30 12:54:14 +0530

    Added user api

---
+++ MagniKanbanWeb/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using MagniKanbanWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagniKanbanWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        public UsersController(UserManager<ApplicationUser> userManager) {
+            this.userManager = userManager;
+        }
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            List<User> users = userManager.Users.Select(u => new User { Id = u.Id, Name = u.Name, Email = u.Email }).ToList();
+            return Ok(users);
+        }
+    }
+}
